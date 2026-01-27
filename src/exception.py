4ea07ccdf0c602aceca602e1c451e07bfe8e32cb
Aastha Rathore <rathoreaasthaspare@gmail.com>
2026-01-27 17:10:46 +0530

    Adding files

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_Details:sys):
    _, _ ,exc_tb= error_Details.exc_info()  
@@ -21,10 +21,5 @@ class customException(Exception):
     def __str__(self):
        return self.error_message
     
-if __name__=="__main__":
-   try:
-      a=1/0
-   except Exception as e:
-      logging.info("Divide by Zero")
-      raise customException(e , sys)
+
    
