5d508c22f913fca0bee4fe723fd2f24e8954fb6c
Aastha Rathore <rathoreaasthaspare@gmail.com>
2026-01-28 22:30:21 +0530

    Other files updated

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os 
 import sys , dill
 from src.exception import customException
-
+from sklearn.metrics import r2_score
 
 def save_object(file_path , obj):
     try:
@@ -20,3 +20,25 @@ def save_object(file_path , obj):
     except  Exception as e:
         raise customException(e , sys)
 
+
+
+def evaluate_model(X_train , y_train , X_test , y_test , models):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+
+            model.fit(X_train , y_train)
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=r2_score(y_train , y_train_pred)
+            test_model_score=r2_score(y_test , y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+            return report
+    except Exception as e:
+        raise customException(e, sys)
+    
